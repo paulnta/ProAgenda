406afa6d7dabdb154bae3cbb388bd25f21661838
Mario Ferreira <mario.jorge.ferreira@gmail.com>
2015-03-31 11:04:22 +0200

    Update repository

---
+++ Conventions de nommage/headerFile.cpp
@@ -0,0 +1,25 @@
+/*****************************************************************************************
+* HEIG-VD // heig-vd.ch
+* Haute Ecole d'Ingenerie et de Gestion du Canton de Vaud
+* School of Business and Engineering in Canton de Vaud
+******************************************************************************************
+*
+* File                 : header.php
+* Author               : prenom nom (NOMP)
+* Email                : prenom.nom@heig-vd.ch
+* Date                 : 31 Mars 2015
+*
+* Project              : Project
+*
+******************************************************************************************
+* Description : What does the code.
+******************************************************************************************
+* Information : 
+******************************************************************************************
+* Modifications :
+* Ver     	Date          Engineer     Comments
+*  1.0.0     24.10.13      NOMP         Original version
+*  1.0.1 	 24.10.14      NOMP		    Modify existing code
+*  1.1.0	 16.12.14	   NOMP	        Add feature
+*  2.0.0     13.01.15	   NOMP			Optimize code
+*****************************************************************************************/
